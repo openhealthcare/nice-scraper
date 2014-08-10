136504fab1bbd0b4e1a165e96ac84b8d1b4929e5
Ross Jones <ross@servercode.co.uk>
2014-08-10 20:13:36 +0100

    Working

--- nice_scraper.py
+++ nice_scraper.py
@@ -1,60 +1,72 @@
 #!/usr/bin/env python
 import collections
 import re
+import copy
 import requests
 import time
 from lxml.html import fromstring
 from urlparse import urljoin
+from picklecache import cache
+import pickle_warehouse.serializers
 
 title_rx = re.compile('(.*)\((.*)\)')
 
 content = collections.defaultdict(list)
 
-def process_page(url, data):
-    print url
-    time.sleep(0.5)
-    html = requests.get(url).content
+
+@cache('~/.http')
+def get(url):
+    return requests.get(url).content
+
+def process_page(href, data):
+    html = get(href)
     page = fromstring(html)
     try:
         href = page.cssselect('.track-event')[0]
     except:
-        print "No download found on ", url
-        import sys; sys.exit(0)
+        return
+
     link = urljoin('http://www.nice.org.uk', href.get('href'))
 
     crumbs = page.cssselect('#guidance-breadcrumb li')
     cat = crumbs[-2].text_content().strip()
 
-    data['url'] = link
+    data['url'] = link.encode('utf-8')
+    data['category'] = cat.decode('latin1').encode('utf-8')
     content[cat].append(data)
 
-html = requests.get("http://www.nice.org.uk/guidance/published?type=Guidelines").content
-page = fromstring(html)
-rows = page.cssselect(".rowlink tr")
-for row in rows:
-    title = row[0][0].text_content()
-    title, code = title_rx.match(title).groups()
-    if not code.strip().startswith('CG'):
-        continue
+def process():
+    html = get("http://www.nice.org.uk/guidance/published?type=Guidelines")
+    page = fromstring(html)
+    rows = page.cssselect(".rowlink tr")
+    for row in rows:
+        title = row[0][0].text_content().encode('utf-8')
+        title, code = title_rx.match(title).groups()
+        if not code.strip().startswith('CG'):
+            continue
 
-    href = urljoin('http://www.nice.org.uk', row[0][0].get('href'))
-    date = row[1].text_content()
-    data = {
-        'code': code,
-        'title': title,
-        'date': date
-    }
-    process_page(href, data)
+        href = urljoin('http://www.nice.org.uk', row[0][0].get('href'))
+        date = row[1].text_content()
+        data = {
+            'code': code.strip(),
+            'title': title.strip().decode('utf-8'),
+            'date': date.strip(),
+        }
+        process_page(href, data)
 
-print content
+process()
+keys = sorted(content.keys())
+print u'<?xml version="1.0" encoding="utf-8"?>'
+print u'<guidelines>'
+print u'<provider name="NICE">'
+for key in keys:
+    print u'<category name="%s">' % key
+    for g in content[key]:
+        print (u'''   <guideline category="%s" code="%s" subcategory="">
+                <url>%s</url>
+                <title>%s</title>
+            </guideline>''' % (g['category'], g['code'], g['url'], g['title'])).encode('utf-8')
 
-
-
-
-"""
-<guidelines><provider name="NICE"><category name="Blood and immune system"><guideline category="Blood and immune system" code="CG114" subcategory=""><url>
-          http://www.nice.org.uk/nicemedia/live/13329/52853/52853.pdf
-        </url><title>
-          Anaemia management in people with chronic kidney disease
-        </title></guideline>
-"""
+    print u'</category>'
+print u'</provider>'
+print u'</guidelines>'
